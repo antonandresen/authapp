faeda4c2cf55e170db0580cdcd350488621f0241
Anton <anton_real_madrid@icloud.com>
2019-10-02 23:25:02 +0200

    added react client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const app = require('./server/app');
+const app = require('./app');
 
 // Start server.
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
